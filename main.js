deb2e8a0856d521b6d24948351c81b91e0d5342c
DevMello <kol.kid.py@gmail.com>
2024-06-25 20:19:07 -0700

    added some tailwind edits including Daisy UI

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    
+    
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: false, // protect against prototype pollution
+      nodeIntegration: true, 
     }
   })
 
--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+const { Ollama } = require("ollama");
+
 /**
  * This file is loaded via the <script> tag in the index.html file and will
  * be executed in the renderer process for that window. No Node.js APIs are
@@ -5,3 +7,35 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+const ollama = new Ollama();
+
+let models = [];
+
+document.addEventListener("DOMContentLoaded", async () => {
+    console.log("Hello from renderer.js");
+    models = await ollama.list();
+    console.log(await ollama.list());
+});
+
+
+function populateSelect(models) {
+
+    const selectElement = document.querySelector('.select');
+  
+
+    selectElement.innerHTML = '';
+  
+    const initialOption = document.createElement('option');
+    initialOption.disabled = true;
+    initialOption.selected = true;
+    initialOption.textContent = 'Select';
+    selectElement.appendChild(initialOption);
+  
+    models.forEach(model => {
+      const option = document.createElement('option');
+      option.value = model.name;
+      option.textContent = model.name; 
+      selectElement.appendChild(option);
+    });
+  }
+  
